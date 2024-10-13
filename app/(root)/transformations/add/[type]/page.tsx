ca1d74fb89036b5b1aa16b87a6f0b020f031a9fd
PixelPetalStudio <160943075+PixelPetalStudio@users.noreply.github.com>
2024-10-13 18:11:02 +0100

    Update page.tsx

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -21,8 +21,8 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
       />
     
       <section className="mt-10">
-        <TransformationForm
-        action="Add"
+        <TransformationForm 
+          action="Add"
           userId={user._id}
           type={transformation.type as TransformationTypeKey}
           creditBalance={user.creditBalance}
